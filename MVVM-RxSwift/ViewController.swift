df3c74e871dbb5c54ad829ed88db0cb560613024
chikari321 <chikari0321@gmail.com>
2021-04-04 13:58:35 +0900

    Initial Commit

---
+++ MVVM-RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-RxSwift
+//
+//  Created by chikari kobayashi on 2021/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
